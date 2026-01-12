a70657d89387d513498fc7ac55665fd5dcb368cc
Brooks Howe <zbrookshowe@gmail.com>
2026-01-12 15:27:30 -0600

    Initialized main code files Included '__init__.py' file to utilize python package structure, added a file describing syntax and git workflow conventions to follow. Wrote vague pseudo-code from meeting today outlining how package will be structured.

---
+++ GUIDELINES.md
@@ -0,0 +1,13 @@
+# Coding Guidelines
+This file is a description of rules and conventions that contributors to this project are expected to follow.
+
+## Syntax conventions
+- Use function template
+- Use `snake_case` for variables and functions
+- Keep lines to a maximum of 100 characters
+- Always comment blocks of code that do one thing
+
+## Git workflow
+- Always pull from main before beginning coding work
+- When working on code, make sure to create a branch and work from there
+- Merge to main branch regularly
---
+++ video_handling.py
@@ -0,0 +1,45 @@
+import pickle as pkl
+import numpy as np
+
+class Video:
+    '''
+    Description:
+    Attributes:
+    '''
+    def __init__(
+        dataset,
+        length
+    ):
+        pass
+
+    def identify_dropped():
+        '''
+        Description:
+        '''
+        pass
+
+    def insert_dropped():
+        '''
+        Description:
+        '''
+        pass
+
+    def sync_to_house():
+        '''
+        Description:
+        '''
+        pass
+
+
+class Scan:
+    '''
+    Description:
+    Attributes:
+    '''
+    def __init__(
+        depth: float,
+        speed: float,
+        videos: list
+    ):
+        pass
+    
